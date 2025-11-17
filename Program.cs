b8a20d8cc77a896ae0ca654bb22c4aa9d0fb5e3a
e.mullayarov <e.mullayarov@voodoo.rocks>
2025-11-17 13:42:01 +0500

    Add gzip compression and static file caching

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "text/html", "text/css", "application/javascript", "application/json" });
+});
+
+builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+{
+    options.Level = CompressionLevel.Fastest;
+});
 
 builder.Services.AddCors(options =>
 {
@@ -12,11 +28,31 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
+
 var app = builder.Build();
 
+app.UseResponseCompression();
+
 app.UseCors("AllowAll");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = ctx =>
+    {
+        ctx.Context.Response.Headers.Append("Cache-Control", "public,max-age=31536000");
+        
+        if (ctx.File.Name.EndsWith(".html"))
+        {
+            ctx.Context.Response.Headers["Cache-Control"] = "public,max-age=3600";
+        }
+    }
+});
+
 app.UseDefaultFiles();
-app.UseStaticFiles();
 app.UseAuthorization();
 app.MapControllers();
 
--- obj/Debug/net9.0/IcuTechLogin.AssemblyInfo.cs
+++ obj/Debug/net9.0/IcuTechLogin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IcuTechLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90aed7d91a9ee61c7d6b57c45e5c63ab8768c02e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1700823697890ac68dfa75d1f7ee571dc3d0bbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("IcuTechLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IcuTechLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
